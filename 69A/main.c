f54ce8dc678c8a5e09aac470b0ebd75aba0aa571
Aria. Kh <khoshnoodaria@gmail.com>
2020-09-02 01:19:01 +0430

    ["Second Task DONE 01:18AM"]

--- 69A/main.c
+++ 69A/main.c
@@ -1,5 +1,20 @@
-int main(int argc, char const *argv[])
+#include <stdio.h>
+
+int main()
 {
-    
+    int Force, x, y, z;
+    int tx = 0 , ty = 0, tz = 0;
+    scanf("%d", &Force);
+    for(int i=0; i<Force;i++){
+        scanf("%d %d %d", &x, &y, &z);
+        tx += x;
+        ty += y;
+        tz += z;
+    }
+
+    if(tx==0 && ty==0 && tz==0)
+        printf("YES");
+    else
+        printf("NO");
     return 0;
 }
